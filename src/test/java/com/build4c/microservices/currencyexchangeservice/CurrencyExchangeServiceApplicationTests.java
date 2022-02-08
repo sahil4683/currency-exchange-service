11f56151a2bb3b50e2c3dbf12d62eebba2c5a5c1
Sahil Patil <43109674+sahil4683@users.noreply.github.com>
2022-02-08 18:51:44 +0530

    Initial commit

---
+++ src/test/java/com/build4c/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.build4c.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
